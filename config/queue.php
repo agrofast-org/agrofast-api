be9bc7f6915d8abd9abb4f5cb35ad820ff2d009d
Murilo Elias <murilo.elias@gmail.com.br>
2025-02-13 20:47:40 -0300

    chore: adpating the system to recive more micro services

--- config/queue.php
+++ config/queue.php
@@ -1,5 +1,18 @@
 <?php
 
+$default = [
+    'driver' => 'rabbitmq',
+    'hosts' => [
+        [
+            'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+            'port' => env('RABBITMQ_PORT', 5672),
+            'user' => env('RABBITMQ_USER', 'guest'),
+            'password' => env('RABBITMQ_PASSWORD', 'guest'),
+            'vhost' => env('RABBITMQ_VHOST', '/'),
+        ],
+    ],
+];
+
 return [
 
     /*
@@ -35,43 +48,45 @@ return [
         ],
 
         'database' => [
-            'driver'       => 'database',
-            'connection'   => env('DB_QUEUE_CONNECTION'),
-            'table'        => env('DB_QUEUE_TABLE', 'framework.jobs'),
-            'queue'        => env('DB_QUEUE', 'default'),
-            'retry_after'  => (int) env('DB_QUEUE_RETRY_AFTER', 90),
+            'driver' => 'database',
+            'connection' => env('DB_QUEUE_CONNECTION'),
+            'table' => env('DB_QUEUE_TABLE', 'jobs'),
+            'queue' => env('DB_QUEUE', 'default'),
+            'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
             'after_commit' => false,
         ],
 
         'beanstalkd' => [
-            'driver'       => 'beanstalkd',
-            'host'         => env('BEANSTALKD_QUEUE_HOST', 'localhost'),
-            'queue'        => env('BEANSTALKD_QUEUE', 'default'),
-            'retry_after'  => (int) env('BEANSTALKD_QUEUE_RETRY_AFTER', 90),
-            'block_for'    => 0,
+            'driver' => 'beanstalkd',
+            'host' => env('BEANSTALKD_QUEUE_HOST', 'localhost'),
+            'queue' => env('BEANSTALKD_QUEUE', 'default'),
+            'retry_after' => (int) env('BEANSTALKD_QUEUE_RETRY_AFTER', 90),
+            'block_for' => 0,
             'after_commit' => false,
         ],
 
         'sqs' => [
-            'driver'       => 'sqs',
-            'key'          => env('AWS_ACCESS_KEY_ID'),
-            'secret'       => env('AWS_SECRET_ACCESS_KEY'),
-            'prefix'       => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
-            'queue'        => env('SQS_QUEUE', 'default'),
-            'suffix'       => env('SQS_SUFFIX'),
-            'region'       => env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'driver' => 'sqs',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
+            'queue' => env('SQS_QUEUE', 'default'),
+            'suffix' => env('SQS_SUFFIX'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
             'after_commit' => false,
         ],
 
         'redis' => [
-            'driver'       => 'redis',
-            'connection'   => env('REDIS_QUEUE_CONNECTION', 'default'),
-            'queue'        => env('REDIS_QUEUE', 'default'),
-            'retry_after'  => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
-            'block_for'    => null,
+            'driver' => 'redis',
+            'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
+            'queue' => env('REDIS_QUEUE', 'default'),
+            'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
+            'block_for' => null,
             'after_commit' => false,
         ],
 
+        'rabbitmq' => $default,
+        'laravel-queue' => ['queue' => env('QUEUE_LARAVEL', 'laravel_queue')] + $default,
     ],
 
     /*
@@ -87,7 +102,7 @@ return [
 
     'batching' => [
         'database' => env('DB_CONNECTION', 'sqlite'),
-        'table'    => 'framework.job_batches',
+        'table' => 'job_batches',
     ],
 
     /*
@@ -104,9 +119,9 @@ return [
     */
 
     'failed' => [
-        'driver'   => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'database' => env('DB_CONNECTION', 'sqlite'),
-        'table'    => 'framework.failed_jobs',
+        'table' => 'failed_jobs',
     ],
 
 ];
