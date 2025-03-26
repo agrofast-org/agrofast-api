7f5e209044ea3d3ed336faf632ba4df90c887c7a
Murilo Elias <murilo.elias@gmail.com.br>
2025-03-26 10:36:01 -0300

    chore: updated php production dockerfile

--- Dockerfile.prod.php
+++ Dockerfile.prod.php
@@ -1,78 +1,28 @@
-FROM php:8.3-fpm-alpine as build
+FROM php:8.3-fpm
 
-RUN apk add --no-cache \
-    zip \
-    libzip-dev \
-    freetype \
-    libjpeg-turbo \
-    libpng \
-    freetype-dev \
-    libjpeg-turbo-dev \
+RUN apt-get update && apt-get install -y \
+    libfreetype-dev \
+    libjpeg62-turbo-dev \
     libpng-dev \
-    nodejs \
-    npm \
-    && docker-php-ext-configure zip \
-    && docker-php-ext-install zip pdo pdo_mysql \
-    && docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/ \
-    && docker-php-ext-install -j$(nproc) gd \
-    && docker-php-ext-enable gd
-
-COPY --from=composer:2.7.6 /usr/bin/composer /usr/bin/composer
-
-WORKDIR /var/www/html
-
-COPY . .
-
-RUN chown -R www-data:www-data /var/www/html \
-    && chmod -R 775 /var/www/html/storage \
-    && chmod -R 775 /var/www/html/bootstrap/cache
-
-RUN composer install --no-dev --prefer-dist \
-    && npm install \
-    && npm run build
-
-RUN chown -R www-data:www-data /var/www/html/vendor \
-    && chmod -R 775 /var/www/html/vendor
-
-FROM php:8.3-fpm-alpine
-
-RUN apk add --no-cache \
-    zip \
+    zlib1g-dev \
     libzip-dev \
-    freetype \
-    libjpeg-turbo \
-    libpng \
-    freetype-dev \
-    libjpeg-turbo-dev \
-    libpng-dev \
-    oniguruma-dev \
-    gettext-dev \
-    freetype-dev \
-    nginx \
-    && docker-php-ext-configure zip \
-    && docker-php-ext-install zip pdo pdo_mysql \
-    && docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/ \
+    unzip \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
     && docker-php-ext-install -j$(nproc) gd \
-    && docker-php-ext-enable gd \
-    && docker-php-ext-install bcmath \
-    && docker-php-ext-enable bcmath \
-    && docker-php-ext-install exif \
-    && docker-php-ext-enable exif \
-    && docker-php-ext-install gettext \
-    && docker-php-ext-enable gettext \
-    && docker-php-ext-install opcache \
-    && docker-php-ext-enable opcache \
-    && rm -rf /var/cache/apk/*
+    && docker-php-ext-install zip \
+    && docker-php-ext-install sockets
 
-# copy files from the build stage
-COPY --from=build /var/www/html /var/www/html
+COPY . /var/www/app
 
-COPY ./docker/nginx.conf /etc/nginx/http.d/default.conf
+WORKDIR /var/www/app
 
-COPY ./docker/php.ini "$PHP_INI_DIR/conf.d/app.ini"
+RUN chown -R www-data:www-data /var/www/app \
+    && chmod -R 775 /var/www/app/storage
 
-WORKDIR /var/www/html
+COPY --from=composer:2.6.5 /usr/bin/composer /usr/local/bin/composer
 
-VOLUME ["/var/www/html/storage/app"]
+RUN composer install --no-plugins --no-scripts || composer update --no-plugins --no-scripts
 
-CMD ["sh", "-c", "nginx && php-fpm"]
+EXPOSE 9000
+
+CMD ["php-fpm"]
