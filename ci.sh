e35c96f0e52b0b5c4563721afba75a052c8a8c9e
iloElias <murilo7456@gmail.com>
2025-04-22 19:03:15 -0300

    fix: update CI script to run queue worker in the background and improve output handling

--- ci.sh
+++ ci.sh
@@ -4,10 +4,8 @@ set -e
 echo "Running database migrations"
 php artisan migrate --force
 
-# echo "Seeding the database"
-# php artisan db:seed --force
-
 echo "Starting Laravel queue worker in background"
-php artisan queue:work &
+nohup php artisan queue:work > /dev/null 2>&1 &
+disown
 
 php artisan serve --host=0.0.0.0 --port=80
